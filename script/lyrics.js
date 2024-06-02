92a3d123dcaa628139f51de573d410b65f2ac74a
ericson_pascua <167420722+Nathanielpradas14@users.noreply.github.com>
2024-06-02 18:53:42 +0800

    Create lyrics.js

---
+++ script/lyrics.js
@@ -0,0 +1,37 @@
+const axios = require('axios');
+const fs = require('fs');
+
+module.exports.config = {
+  name: 'lyrics',
+  version: '1',
+  role: 0,
+  credits: 'Grey',
+  hasPrefix: true,
+  description: 'Lyrics Finder',
+  commandCategory: 'fun',
+  usage: '[song]',
+  cooldowns: 5
+};
+
+module.exports.run = async ({ api, event, args }) => {
+  api.setMessageReaction("⏳", event.messageID, (err) => {
+  }, true);
+api.sendTypingIndicator(event.threadID, true);
+  const song = args.join(' ');
+
+  if (!song) {
+    return api.sendMessage('Please enter a song.', event.threadID, event.messageID);
+  } else {
+    axios.get(`https://lyrist-tumk.onrender.com/api/${encodeURIComponent(song)}`)
+      .then(res => {
+        const { lyrics, title, artist } = res.data;
+
+        const message = `Title: ${title}\n\nArtist: ${artist}\n\nLyrics: ${lyrics}`;
+        api.sendMessage(message, event.threadID, event.messageID);
+      })
+      .catch(error => {
+        console.error('Lyrics API error:', error);
+        api.sendMessage('Failed to fetch lyrics.', event.threadID, event.messageID);
+      });
+  }
+};
